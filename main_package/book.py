8f257f1cce895beb7756cf25d8319d2819e1200c
Julles Pio <jullespio@tutanota.com>
2025-05-06 16:15:42 -0300

    Class Book created, basic code added

---
+++ main_package/book.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class Book:
+    def __init__(self, title, author, publisher=None, year=None, isbn=None,
+                 rating=None, other_info=None, read=False, lent=False):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        self.isbn = isbn
+        self.rating = rating  # e.g., a float or int (0-5)
+        self.other_info = other_info  # any additional information
+        self.read = read
+        self.lent = lent
+        self.date_created = datetime.now()
+        self.date_modified = datetime.now()
+
+    # The metghod below updates an object's attributes using keyword arguments, only if those 
+    # attributes already exist, and then sets a date_modified attribute to the current time.
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.date_modified = datetime.now()
+
+    def __repr__(self):
+        return (f"Book(title='{self.title}', author='{self.author}', "
+                f"publisher='{self.publisher}', year={self.year}, isbn='{self.isbn}', "
+                f"rating={self.rating}, read={self.read}, lent={self.lent}, "
+                f"date_created={self.date_created}, date_modified={self.date_modified})")
