a5205985fff182dbd3c2f7241a20a7385bc7b76e
Julles Pio <jullespio@tutanota.com>
2025-05-16 16:33:20 -0300

    Setup and requirements updated

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# Load requirements.txt
+def load_requirements(filename="requirements.txt"):
+    with open(filename, encoding="utf-8") as f:
+        lines = f.readlines()
+        return [line.strip() for line in lines if line.strip() and not line.startswith("#")]
+
 setup(
     name="tinyshelf2",
     version="2.1.0",
@@ -7,16 +13,12 @@ setup(
     author="Julles",
     packages=find_packages(where="tinyshelf2"),
     package_dir={"": "tinyshelf2"},
-    include_package_data=True,
-    install_requires=[
-        line.strip()
-        for line in open("tinyShelf2/requirements.txt").readlines()
-        if line.strip() and not line.startswith("#")
-    ],
+    install_requires=load_requirements(),
     entry_points={
         "console_scripts": [
             "tinyshelf2=tinyshelf2.cli:run"
         ]
     },
+    include_package_data=True,
     python_requires=">=3.7",
 )
