067e2b2aaf9d842f8b11f6e30d9047cb67a05573
Julles Pio <jullespio@tutanota.com>
2025-05-23 17:21:17 -0300

    Imports updated in main_app

--- tinyshelf2/main_app.py
+++ tinyshelf2/main_app.py
@@ -1,9 +1,9 @@
 # Import the helpers from the current folder "."
-from .utils import display_greeting
+from tinyshelf2.utils.utils import display_greeting
 
 # Import the subpackage_module
 # from .subpackage import subpackage_module
-from ..prompts.main_prompt import CLIMenuInterface
+from tinyshelf2.prompts.main_prompt import CLIMenuInterface
 
 def main_module_function():
         main = CLIMenuInterface()
